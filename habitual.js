64ba39a6e0366c7cfdce31fc3e9e834817f8e3d6
Andrea Soper <github@zendoodles.com>
2015-01-24 11:35:42 -0600

    Tutorial end of step 3.

--- habitual.js
+++ habitual.js
@@ -1,17 +1,9 @@
+Habits = new Mongo.Collection("habits");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    habits: function () {
+      return Habits.find({});
     }
   });
 }
